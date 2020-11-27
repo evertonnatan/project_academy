dd902ce606e568c8b7fad596932bc2f1e903ac44
danteaquiles <evertonnatandl@outlook.com>
2020-11-27 07:59:03 -0300

    Adicionando create_at e birth ao data.json

--- instructors.js
+++ instructors.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const data = require("./data.json")
 
 // create
 exports.post = function(req, res) {
@@ -10,8 +11,12 @@ exports.post = function(req, res) {
             return res.send('Please, fill all fields!')
         }
     }
+    req.body.birth = Date.parse(req.body.birth)
+    req.body.created_at = Date.now()
 
-    fs.writeFile("data.json", JSON.stringify(req.body), function(err){
+    data.instructors.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if (err) return res.send("Write file error!")
 
         return res.redirect("instructors")

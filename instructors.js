bd62fe5e4e2ac89acd445ee8f2cbb8e707e868fa
danteaquiles <evertonnatandl@outlook.com>
2020-12-01 22:12:02 -0300

    Inserindo e estilizando tabela de instrutores, adicionando e ajustando imagens de avatares

--- instructors.js
+++ instructors.js
@@ -2,6 +2,9 @@ const fs = require('fs')
 const data = require("./data.json")
 const { age, date } = require ('./utils')
 
+exports.index = function(req, res) {
+    return res.render("instructors/index", {instructors: data.instructors})
+}
 // show
 exports.show = function(req, res) {
     const { id } = req.params
@@ -100,7 +103,8 @@ exports. put = function(req, res) {
     const instructor = {
         ...foundInstructor,
         ...req.body,
-        birth: Date.parse(req.body.birth)
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
     }
 
     data.instructors[index] = instructor
